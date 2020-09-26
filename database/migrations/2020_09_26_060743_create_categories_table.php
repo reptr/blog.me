4cefab695131c2e5c92594c08ba0b28e5a309fef
restuhanputra <restu.hanputraa@gmail.com>
2020-09-26 22:04:38 +0700

    category migrate

--- database/migrations/2020_09_26_060743_create_categories_table.php
+++ database/migrations/2020_09_26_060743_create_categories_table.php
@@ -15,7 +15,7 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->bigInteger('parent_id')->nullable();
+            $table->unsignedBigInteger('parent_id')->nullable();
             $table->string('name');
             $table->string('slug');
             $table->timestamps();
