c0aee38dcedb405f657689028fbb5c0e64017569
restuhanputra <restu.hanputraa@gmail.com>
2020-09-28 04:36:57 +0700

    add migration images, galleries, tags & taggables

--- database/migrations/2020_09_26_153722_create_images_table.php
+++ database/migrations/2020_09_26_153722_create_images_table.php
@@ -23,7 +23,7 @@ class CreateImagesTable extends Migration
             $table->string('small')->nullable();
             $table->timestamps();
 
-            $table->index(['imageable_id', 'image_type']);
+            $table->index(['imageable_id', 'imageable_type']);
         });
     }
 
--- database/migrations/2020_09_27_201131_create_tags_table.php
+++ database/migrations/2020_09_27_201131_create_tags_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateGalleriesTable extends Migration
+class CreateTagsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,12 @@ class CreateGalleriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('galleries', function (Blueprint $table) {
+        Schema::create('tags', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('slug');
             $table->timestamps();
 
-            $table->foreignId('user_id')->constrained();
             $table->index('slug');
         });
     }
@@ -31,6 +30,6 @@ class CreateGalleriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('galleries');
+        Schema::dropIfExists('tags');
     }
 }
