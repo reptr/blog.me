265cc4e9556a42acde0e11c7c98bdcb42ae243ca
restuhanputra <restu.hanputraa@gmail.com>
2020-09-27 18:20:20 +0700

    add many to many & polymorphic one to many

---
+++ app/Models/Post.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    // one to many user punya banyak post
+    public function user()
+    {
+       return $this->belongsTo('App\Models\User');
+    }
+
+    // many-to-many dgn category
+    public function categories()
+    {
+       return $this->belongsToMany('App\Models\Category');
+    }
+
+    // polymorphic one to many post dengan image
+    public function images()
+    {
+       return $this->morphMany('App\Models\Image', 'imageable');
+    }
+}
