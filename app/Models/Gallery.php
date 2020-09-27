5eae007df701553bd59da5901fcb3474f05374fa
restuhanputra <restu.hanputraa@gmail.com>
2020-09-28 04:35:37 +0700

    add relation image & tags

--- app/Models/Gallery.php
+++ app/Models/Gallery.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Gallery extends Model
 {
     use HasFactory;
+
+    // polymorphic one to many gallery dengan image
+    public function images()
+    {
+       return $this->morphMany('App\Models\Image', 'imageable');
+    }
+
+    // polymorphic one to many gallery dengan tag
+    public function tags()
+    {
+       return $this->morphToMany('App\Models\Tag', 'taggable');
+    }
 }
